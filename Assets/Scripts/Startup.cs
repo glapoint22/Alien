0f593daf963d6483022ec7f6b5bf3f244ff81341
glapoint22 <glapoint22@gmail.com>
2016-09-03 12:09:53 -0400

    Setup Development/Live

--- Assets/Scripts/Startup.cs
+++ Assets/Scripts/Startup.cs
@@ -1,21 +1,40 @@
 using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 
 public class Startup : MonoBehaviour
 {
+    [SerializeField]
+    GameObject spaceBackgroundPrefab;
+
+    [SerializeField]
+    private Version version;
+    private enum Version { Development, Live}
+
     IEnumerator Start()
     {
-        AssetBundles assetBundles = new AssetBundles();
+        if(version == Version.Live)
+        {
+            AssetBundles assetBundles = new AssetBundles();
 
-        //Load the space background
-        yield return assetBundles.LoadGameObjectFromAssetBundle("space_background", "Space Background");
-        DontDestroyOnLoad(assetBundles.asset);
+            //Load the space background
+            yield return assetBundles.LoadGameObjectFromAssetBundle("space_background", "Space Background");
+            DontDestroyOnLoad(assetBundles.asset);
 
-        //Download the asset bundles
-        yield return assetBundles.DownloadAssetBundles();
+            //Download the asset bundles
+            yield return assetBundles.DownloadAssetBundles();
 
-        //Load the login scene
-        yield return assetBundles.LoadScene("scenes/login", "Login", false);
+            //Load the login scene
+            yield return assetBundles.LoadScene("scenes/login", "Login", false);
+        }
+        else
+        {
+            GameObject spaceBackground = Instantiate(spaceBackgroundPrefab);
+            DontDestroyOnLoad(spaceBackground);
+            SceneManager.LoadScene(1);
+        }
     }
 }
+
+
